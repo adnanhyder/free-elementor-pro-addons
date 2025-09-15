31d3e3c94f4f611917dd9fd55f87fa927cc9c61b
adnan <12345adnan@gmail.com>
2025-09-15 20:22:47 +0500

    standered of wp

--- assets/js/admin.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function($) {
-	$('.xpert-admin-notice').css('color', '#0073aa');
-});
--- assets/js/frontend.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function($) {
-	$('.xpert-custom').text('Enhanced by xpert Core!');
-});
